f886ff7d494ab51f7e4cc1b133a3f6760cd9e4b0
chandrasekhara03 <chandrasekhara176@gmail.com>
2026-06-09 10:45:08 +0700

    fix: update vite base path to repository name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/', // 👈 Wajib pakai format ini agar jalurnya mengarah ke folder repositori Anda di GitHub
 })
